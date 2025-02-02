de21c2b1cff0784097b723792e3015a40f9e480b
yahya-mohamoud <yahyamohamoud625@gmail.com.com>
2025-02-02 11:31:18 +0300

    re-created the readbtn

--- script.js
+++ script.js
@@ -19,15 +19,14 @@ function addBookToLibrary() {
   const author = document.querySelector("#author").value;
   const pages = document.querySelector("#pages").value;
   const read = document.querySelector("#status").checked;
-  const readStatus = read ? "Read" : "Not Read Yet";
-  
+  const readStatus = read ? "Read" : "Not Read";
+
 
   const book = new Book(title, author, pages, readStatus);
   myLibrary.push(book);
 }
 
 function showBooks() {
-  
   const bookOne = document.createElement("div")
   bookOne.classList.add("book")
 
@@ -41,16 +40,16 @@ function showBooks() {
   bookOne.appendChild(bookAuthor)
 
   const bookPages = document.createElement('h2')
-  bookPages.innerText= `Pages: ${book1.pages}`
+  bookPages.innerText = `Pages: ${book1.pages}`
   bookOne.appendChild(bookPages)
 
   const bookStatus = document.createElement('h2')
-  bookStatus.innerText= `Status: ${book1.status}`
+  bookStatus.innerText = `Status: ${book1.status}`
   bookOne.appendChild(bookStatus)
 
   const readBtn = document.createElement('button')
   readBtn.classList.add('read')
-  readBtn.innerText = 'Read'
+  readBtn.innerText = `Read`
   const btnOne = document.createElement('button')
   btnOne.classList.add('delete')
   btnOne.innerText = 'Delete'
@@ -59,20 +58,85 @@ function showBooks() {
   bookOne.appendChild(btnOne)
   bookList.appendChild(bookOne)
   myLibrary.push(book1)
+
+  handleDelete(btnOne)
+  handleStatus(readBtn, bookStatus)
 }
 
 showBooks()
 
 function display() {
+  bookList.innerText = ''
+  myLibrary.forEach((book) => {
+    const books = document.createElement('div')
+    books.classList.add('book')
 
+    const bookTitle = document.createElement('h2')
+    bookTitle.innerText = `Title: ${book.title}`
+    books.appendChild(bookTitle)
+
+    const bookAuthor = document.createElement('h2')
+    bookAuthor.innerText = `Author: ${book.author}`
+    books.appendChild(bookAuthor)
+
+    const bookPages = document.createElement('h2')
+    bookPages.innerText = `Pages: ${book.pages}`
+    books.appendChild(bookPages)
+
+    const bookStatus = document.createElement('h2')
+    bookStatus.innerText = `Status: ${book.status}`
+    books.appendChild(bookStatus)
+
+    const readBtn = document.createElement('button')
+    readBtn.classList.add('read')
+    if (book.status == "Read") {
+        readBtn.textContent = "Not Read";
+        readBtn.style.background = '#ba0707'
+    } else {
+      readBtn.textContent = "Read"
+      readBtn.style.background = 'green'
+
+    }
+    // readBtn.innerText = ""
+
+   const deleteBtn = document.createElement('button')
+    deleteBtn.classList.add('delete')
+    deleteBtn.innerText = 'Delete'
+
+    books.appendChild(readBtn)
+    books.appendChild(deleteBtn)
+    bookList.appendChild(books)
+
+    handleDelete(deleteBtn)
+    handleStatus(readBtn, bookStatus)
+  })
 }
 
-const one = new Book("cali", "cali", 23, "read")
+const one = new Book("cali", "cali", 23, "Read")
 myLibrary.push(one)
 
 
-function display() {
-  // const 
+function handleDelete(btn) {
+  btn.addEventListener('click', (index) => {
+    myLibrary.splice(index, 0)
+    console.log("woekin");
+    // display()
+  })
+}
+
+function handleStatus(btn, status) {
+  btn.addEventListener('click', () => {
+    if (btn.textContent == "Read") {
+      btn.textContent = "Not Read"
+      status.innerText = `Status: ${" Read"}`
+      btn.style.backgroundColor = '#ba0707'
+    } else  {
+      btn.textContent = "Read"
+      status.innerText = `Status: ${"Not Read"}`
+      btn.style.backgroundColor = 'green'
+
+    }
+  })
 }
 
 addBook.addEventListener("click", () => {
@@ -84,12 +148,12 @@ closeBtn.addEventListener("click", () => {
 });
 
 confirmBtn.addEventListener("click", (e) => {
- addBookToLibrary();
   addBookToLibrary();
   e.preventDefault();
+  display()
   dialog.close();
 });
 
 
-
 console.log(myLibrary);
+
